daa87f741e46a1922852a24ed8c08de64f3b1da5
Koby Smith-McCallister <kemccallister2002@gmail.com>
2023-05-19 19:16:31 -0500

    Added the files to complete the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+// src/App.js
 
-function App() {
-  return 
-    <div>Hello</div>; 
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <main className = "text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
-
-export default App;
