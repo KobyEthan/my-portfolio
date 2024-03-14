c001576a20ade3f297e8ece18643ce42ef1e5b23
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-14 12:25:15 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+export default function App() {
+  return (
+    <main className="text-grey-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
